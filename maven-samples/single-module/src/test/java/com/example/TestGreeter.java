fa1e574b6b6833ec3d6905d5bd932b18ded7c7d9
harishdevops07@gmail.com <harishdevops07@gmail.com>
2021-01-17 15:36:58 +0530

    updated the greetShouldIncludeGreetingMessage method

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -33,7 +33,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy 123 456 789";
+    String someone = "World, Thanks you making me Happy 123";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
